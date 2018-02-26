e4fc4aec96c330d8f8918c3360f427db9833d3e3
Inkyu Sa <enddl22@gmail.com>
2018-02-26 22:31:45 +0100

    Change paths.

--- my_scripts/train_scientifica.sh
+++ my_scripts/train_scientifica.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-../caffe-segnet-cudnn5/tools/caffe train -gpu 1 -solver ../SegNet-Tutorial/Models/segnet_scientifica_solver.prototxt -weights ../weights/VGG_ILSVRC_16_layers.caffemodel
+../caffe-segnet-cudnn5/tools/caffe train -gpu 0 -solver ../SegNet-Tutorial/Models/segnet_scientifica_solver.prototxt -weights ../weights/VGG_ILSVRC_16_layers.caffemodel
