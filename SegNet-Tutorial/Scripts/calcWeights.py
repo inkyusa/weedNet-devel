0a0726ade83c898e5fe79012ed34b41f7fc11a77
inkyusa <enddl22@gmail.com>
2019-05-16 14:14:08 +0200

    add pixel ratio calculation routine

--- SegNet-Tutorial/Scripts/calcWeights.py
+++ SegNet-Tutorial/Scripts/calcWeights.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+from __future__ import print_function
 import numpy as np
 import argparse
 import os
@@ -5,6 +7,7 @@ from PIL import Image
 from os import listdir
 import sys
 import collections
+
 
 # Import arguments
 # python ./calcWeights.py --dir /home/enddl22/workspace/SegNet-Tutorial/SequoiaMulti/trainannot
@@ -21,8 +24,13 @@ else:
 image_names = listdir(cwd)
 # Keep only images and append image_names to directory
 image_list = [cwd + s for s in image_names if s.lower().endswith(('.png', '.jpg', '.jpeg'))]
-
-print "Number of images:", len(image_list)
+del image_list[3:]
+print ("Number of images:", len(image_list))
+img=Image.open(image_list[0])
+img_w=img.size[0]
+img_h=img.size[1]
+print("img_w=",img_w)
+print("img_h=",img_h)
 
 def count_all_pixels(image_list):
     dic_class_imgcount = dict()
@@ -37,7 +45,7 @@ def count_all_pixels(image_list):
             # If the class is present in the image, then increase the value by one
             # shows in how many images a particular class is present
             dic_class_imgcount[key] = dic_class_imgcount.get(key, 0) + 1
-    print "Done"
+    print ("Done")
     # Save above 2 variables in a list
     for (k, v), (k2, v2) in zip(overall_pixelcount.items(), dic_class_imgcount.items()):
         if k != k2: print ("This was impossible to happen, but somehow it did"); exit()
@@ -49,32 +57,61 @@ def get_class_per_image(img):
     dic_class_pixelcount = dict()
     im = Image.open(img)
     pix = im.load()
-    for x in range(im.size[0]):
-        for y in range(im.size[1]):
+    for x in range(img_w):
+        for y in range(img_h):
             dic_class_pixelcount[pix[x, y]] = dic_class_pixelcount.get(pix[x, y], 0) + 1
     #del dic_class_pixelcount[11]
     return dic_class_pixelcount
+
+def cal_class_weights_deeplab(data):
+    total_num_pixel=img_h * img_w * len(image_list)
+    cls_num_pixel=[]
+    for i,(key,val) in enumerate(data.items()):
+        #print(val)
+        cls_num_pixel.append(val[0])
+    min_cls_num_pixel=min(cls_num_pixel)
+    cls_weight=[]
+    for pixel in cls_num_pixel:
+        cls_weight.append(pixel/min_cls_num_pixel)
+    print("=========================================")
+    print(" pixel ratio, normalized by the number \n")
+    print(" of pixels of the least frequent class")
+    print(" Note that this is for deeplab loss weight")
+    print("=========================================")
+    for i, v in enumerate(cls_weight):
+        print ("    class", i, "weight:", round(v, 4))
+    print("Copy this")
+    print("label_weights=", cls_weight)
+    print("\n\n\n")
 
 
 def cal_class_weights(image_list):
     freq_images = dict()
     weights = collections.OrderedDict()
     # calculate freq per class
-    for k, (v1, v2) in count_all_pixels(image_list).items():
-        freq_images[k] = v1 / (v2 * 360 * 480 * 1.0)
+    data_statistic=count_all_pixels(image_list)
+    cal_class_weights_deeplab(data_statistic)
+    for k, (v1, v2) in data_statistic.items():
+        freq_images[k] = v1 / (v2 * img_h * img_w * 1.0)
     # calculate median of freqs
     median = np.median(freq_images.values())
+    print('median=',median)
     # calculate weights
     for k, v in freq_images.items():
         weights[k] = median / v
+        #print('k=',k)
+        #print('v=',v)
     return weights
 
 results = cal_class_weights(image_list)
 
+print("=========================================")
+print(" Frequency ratio for SegNet            \n")
+print("=========================================")
 # Print the results
 for k, v in results.items():
-    print "    class", k, "weight:", round(v, 4)
+    print("    class", k, "weight:", round(v, 4))
 
-print "Copy this:"
+print("Copy this:")
 for k, v in results.items():
-    print "    class_weighting:", round(v, 4)
+    print("    class_weighting:", round(v, 4))
